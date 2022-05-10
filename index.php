dee05842ee5e5087cab7fd732fd4d16dfde63183
Nurai <nurainurgalieva22@gmail.com>
2022-05-10 13:04:38 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>NIX Education</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<h1></h1>
+<?php
+echo "Hello, NIX Education";
+?>
+<script src="script.js"></script>
+</body>
+</html>
